43f160268ca5b3455cee1cd1cb3bc8e34bcada4b
Naveenkumar <naveensg.23@gmail.com>
2022-12-05 14:17:50 +0530

    drop down filter fixed

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useReducer, useState } from "react";
-
 import axios from "axios";
 import logger from "use-reducer-logger";
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Product from "../components/Product";
-import {Helmet} from "react-helmet-async";
+import { Helmet } from "react-helmet-async";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -17,17 +16,13 @@ const reducer = (state, action) => {
       return { ...state, loading: false, error: action.payload };
     default:
       return state;
-      // JGIPJ
   }
 };
 
 function HomeScreen() {
   // const [Products, setProducts] = useState([]);
   const [data, setdata] = useState([]);
-  const [dummy, setDummy] = useState([]);
-  const [currentValue, setcurrentValue] = useState('');
-
-
+  const [currentValue, setcurrentValue] = useState("");
 
   const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
     products: [],
@@ -35,10 +30,9 @@ function HomeScreen() {
     error: "",
   });
 
- 
   const changeValue = (value) => {
-    setcurrentValue(value)
-  }
+    setcurrentValue(value);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -57,54 +51,37 @@ function HomeScreen() {
     fetchData();
   }, []);
 
-  // 
+  //
 
-  
   useEffect(() => {
     let newData;
     let tempData = [...data];
-    let tempDummyData = [...dummy];
-    if(currentValue === "lowest"){
-      if(dummy.length == 0){
-        newData = tempData.sort((a, b) => a.price - b.price);
-        setdata(newData);
-      }
-      if(dummy.length !== 0){
-        newData = tempDummyData.sort((a, b) => a.price - b.price);
-        setDummy(newData);
-      }
+
+    if (currentValue === "lowest") {
+      newData = tempData.sort((a, b) => a.price - b.price);
+
+      dispatch({ type: "FETCH_SUCCESS", payload: newData });
     }
-    if(currentValue === "highest"){
-      if(dummy.length == 0){
-        newData = tempData.sort((a, b) => b.price - a.price);
-        setdata(newData);
-      }
-      if(dummy.length !== 0){
-        newData = tempDummyData.sort((a, b) => b.price - a.price);
-        setDummy(newData);
-      }
+    if (currentValue === "highest") {
+      newData = tempData.sort((a, b) => b.price - a.price);
+
+      dispatch({ type: "FETCH_SUCCESS", payload: newData });
     }
-    if(currentValue === "a - z"){
-      if(dummy.length == 0){
-        newData = tempData.sort((a, b)=> a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1);
-        setdata(newData);
-      }
-      if(dummy.length !== 0){
-        newData = tempDummyData.sort((a, b)=> a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1);
-        setDummy(newData);
-      }
+    if (currentValue === "a - z") {
+      newData = tempData.sort((a, b) =>
+        a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1
+      );
+
+      dispatch({ type: "FETCH_SUCCESS", payload: newData });
     }
-    if(currentValue === "z - a"){
-      if(dummy.length == 0){
-        newData = tempData.sort((a, b)=> a.name.toLowerCase() < b.name.toLowerCase() ? 1 : -1);
-        setdata(newData);
-      }
-      if(dummy.length !== 0){
-        newData = tempDummyData.sort((a, b)=> a.name.toLowerCase() < b.name.toLowerCase() ? 1 : -1);
-        setDummy(newData);
-      }
+    if (currentValue === "z - a") {
+      newData = tempData.sort((a, b) =>
+        a.name.toLowerCase() < b.name.toLowerCase() ? 1 : -1
+      );
+
+      dispatch({ type: "FETCH_SUCCESS", payload: newData });
     }
-  }, [currentValue]); 
+  }, [currentValue]);
 
   return (
     <div>
@@ -112,31 +89,27 @@ function HomeScreen() {
         <title>Cognizant</title>
       </Helmet>
       <Row>
-        <Col> <h1>Find your products...</h1></Col>
         <Col>
-        {
-            dummy.length == 0 && 
-            <span>{data.length} results</span>
-          }
-          {
-            dummy.length != 0 && 
-            <span>{dummy.length} results</span>
-          }
-        <form>
-            <select 
-                onChange={(event) => changeValue(event.target.value)}
-                value={currentValue}
-              >
+          {" "}
+          <h1>Find your products...</h1>
+        </Col>
+        <Col>
+          <form>
+            <select
+              onChange={(event) => changeValue(event.target.value)}
+              value={currentValue}
+            >
               <option>All Products</option>
               <option value="lowest">price(lowest)</option>
               <option value="highest">price(highest)</option>
               <option value="a - z">a - z</option>
               <option value="z - a">z - a</option>
             </select>
-          </form></Col>
+          </form>
+        </Col>
       </Row>
-     
-      <br/>
+
+      <br />
       <div className="products">
         {loading ? (
           <div>Loading...</div>
@@ -145,7 +118,7 @@ function HomeScreen() {
         ) : (
           <Row>
             {products.map((product) => (
-              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3" >
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
                 <Product product={product}></Product>
               </Col>
             ))}
